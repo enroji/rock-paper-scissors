8424761a1bf29e9ebda9a526e29ea6f6ff035770
enroji <enroji.cr@gmail.com>
2022-04-11 21:31:28 -0600

    Add game function

--- gameLogic.js
+++ gameLogic.js
@@ -7,31 +7,33 @@ function computerPlay() {
 
   return choice[randomNumber];
 }
-
 // Plays a game round
-function gameRound(playerSelection, computerSelection) {
+function playRound(playerSelection, computerSelection) {
+  playerSelection = playerSelection.toUpperCase();
+  computerSelection = computerSelection.toUpperCase();
+  // if both make the same choice
   if (playerSelection === computerSelection) {
-    return `Tie! ${playerSelection} ${computerSelection}`;
+    return "Tie!";
   }
-  // if the player selects rock
-  else if (playerSelection.toUpperCase() === "ROCK") {
-    if (computerSelection === "Paper") {
+  // if the player selects paper
+  else if (playerSelection === "ROCK") {
+    if (computerSelection === "PAPER") {
       return "You lose! Paper beats Rock";
     } else {
       return "You win! Rock beats Scissors";
     }
   }
   // if the player selects paper
-  else if (playerSelection.toUpperCase() === "PAPER") {
-    if (computerSelection === "Scissors") {
+  else if (playerSelection === "PAPER") {
+    if (computerSelection === "SCISSORS") {
       return "You lose! Scissors beat Paper";
     } else {
       return "You win! Paper beats Rock";
     }
   }
   // if the player selects scissors
-  else if (playerSelection.toUpperCase() === "SCISSORS") {
-    if (computerSelection === "Rock") {
+  else if (playerSelection === "SCISSORS") {
+    if (computerSelection === "ROCK") {
       return "You lose! Rock beats Paper";
     } else {
       return "You win! Paper beats Rock";
@@ -42,3 +44,12 @@ function gameRound(playerSelection, computerSelection) {
     return "Enter a valid choice!";
   }
 }
+// Plays the game for five rounds
+function game() {
+  for (let i = 0; i < 5; i++) {
+    const choice = prompt("Type: Rocks, Paper or Scissors");
+    alertrock(playRound(choice, computerPlay()));
+  }
+}
+
+game();
